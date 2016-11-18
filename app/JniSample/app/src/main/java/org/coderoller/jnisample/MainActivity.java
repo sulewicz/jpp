26260e9f74b9d0803ff06cf785810da468d1f1c3
Szymon Ulewicz <szymon.ulewicz@gmail.com>
2016-11-18 22:30:40 +0100

    Sample project created.

---
+++ app/JniSample/app/src/main/java/org/coderoller/jnisample/MainActivity.java
@@ -0,0 +1,24 @@
+package org.coderoller.jnisample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+
+    public native String stringFromJNI();
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+}
